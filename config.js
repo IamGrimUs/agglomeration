313f13cb2ceb52d9ce281b02833dd46fae7a7d93
Russell Thomas <iamgrimus@gmail.com>
2017-09-22 20:12:56 -0700

    config.js variables moved to local env and heroku

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ exports.DATABASE_URL =
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-agglomeration';
 exports.PORT = process.env.PORT || 8080;
-exports.JWT_SECRET = 'jabawakey';
+exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '1d';
 exports.CLOUDINARY_SECRET = process.env.CLOUDINARY_SECRET;
 exports.CLOUDINARY_KEY = process.env.CLOUDINARY_KEY;
